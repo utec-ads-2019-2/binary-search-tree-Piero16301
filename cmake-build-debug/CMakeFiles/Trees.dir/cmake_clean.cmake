03f51ceddb866e949254f0102c6b401a72ab373a
Piero Angelo <piero.morales@utec.edu.pe>
2019-09-18 18:29:53 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Trees.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trees.dir/main.cpp.obj"
+  "Trees.pdb"
+  "Trees.exe"
+  "Trees.exe.manifest"
+  "libTrees.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
